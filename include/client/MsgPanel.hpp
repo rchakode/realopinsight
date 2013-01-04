41c00c28ba9f96c2a16c3794e9f9214be5881cc1
Chakode <rodrigue.chakode@gmail.com>
2013-01-04 22:06:05 +0100

    cleaning code

--- include/client/MsgPanel.hpp
+++ include/client/MsgPanel.hpp
@@ -39,22 +39,10 @@ protected:
   {
     QVariant leftData = sourceModel()->data(left, Qt::UserRole);
     QVariant rightData = sourceModel()->data(right, Qt::UserRole);
-
-    //qDebug() << leftData.toString() << "< " << rightData.toString();
     if (leftData.type() == QVariant::DateTime) {
         return leftData.toDateTime() < rightData.toDateTime();
       } else {
-        //          QRegExp *emailPattern = new QRegExp("([\\w\\.]*@[\\w\\.]*)");
-
-        QString leftString = leftData.toString();
-        //          if(left.column() == 1 && emailPattern->indexIn(leftString) != -1)
-        //            leftString = emailPattern->cap(1);
-
-        QString rightString = rightData.toString();
-        //          if(right.column() == 1 && emailPattern->indexIn(rightString) != -1)
-        //            rightString = emailPattern->cap(1);
-
-        return QString::localeAwareCompare(leftString, rightString) < 0;
+        return QString::localeAwareCompare(leftData.toString(), rightData.toString()) < 0;
       }
   }
 };
