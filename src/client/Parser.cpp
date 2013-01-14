2ef2d1b52e262ec83d1d58e66026e7fb559a1276
roch-win <rodrigue.chakode@gmail.com>
2013-01-14 21:14:48 +0100

    compute container.end() before starting the loops

--- src/client/Parser.cpp
+++ src/client/Parser.cpp
@@ -122,7 +122,7 @@ void Parser::updateNodeHierachy(NodeListT& _bpnodes,
                                 QString& _graphContent)
 {
   _graphContent = "\n";
-  for (NodeListT::ConstIterator node = _bpnodes.begin();
+  for (auto node = _bpnodes.begin(), end = _bpnodes.end();
        node != _bpnodes.end(); ++node)
   {
     QString nname = node->name;
@@ -140,7 +140,7 @@ void Parser::updateNodeHierachy(NodeListT& _bpnodes,
     }
   }
 
-  for (NodeListT::ConstIterator node = _cnodes.begin();
+  for (auto node = _cnodes.begin(), end = _cnodes.end();
        node != _cnodes.end(); ++node)
   {
     QString nname = node->name;
@@ -163,6 +163,7 @@ void Parser::buildNodeTree(const NodeListT& _bpnodes,
   {
     if (node->child_nodes.isEmpty()) continue;
     auto treeItem = _tree.find(node->id);
+      foreach (const QString& id, ids) {
           auto treeItem = _tree.find(node->id);
     if (treeItem == _tree.end()) {
       utils::alert(QObject::tr("Service not found (%1)").arg(node->name));
