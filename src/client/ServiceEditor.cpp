7b50d2391502816a6e9fb2186dde0548a75d9466
Chakode <rodrigue.chakode@gmail.com>
2013-03-23 16:48:54 +0100

    typos

--- src/client/ServiceEditor.cpp
+++ src/client/ServiceEditor.cpp
@@ -297,12 +297,12 @@ void ServiceEditor::loadCheckField(void)
   mlayout->addWidget(checkField(), mlayoutRowIndex, 1, 1, 2);
   checkField()->setEditable(true);
   help->setToolTip(tr("This depends on your monitoring configuration:"
-                      "\n * For Nagios this follows the patterns 'host_name/service_name' or 'host_name'"
+                      "\n * For Nagios this follows the patterns 'host_name/service_name' or just 'host_name'"
                       "\n    E.g. mysql-server.example.com/Current Load, mysql-server.example.com"
-                      "\n    NOTE: When only host_name is set, the data point is associated to ping status."
+                      "\n    When only host_name is set, the data point is associated to ping status."
                       "\n * For Zabbix it follows the pattern 'host_name/trigger_name'"
                       "\n    E.g. Zabbix server/Zabbix http poller processes more than 75% busy"
-                      "\n * For Zenoss it follows the patterns 'device_name/component_name' or 'device_name'"
+                      "\n * For Zenoss it follows the patterns 'device_name/component_name' or just 'device_name'"
                       "\n    E.g. localhost/httpd, localhost"
                       "\n    NOTE: When only device_name is set, the data point is associated to ping status."
                       "\nSee the online documentation for further details."
