7efdb426f81421a9a57845ca3f5aadfc429b6dbc
Chakode <rodrigue.chakode@gmail.com>
2013-03-26 20:32:39 +0100

    removed unnecessary messages

--- src/client/utilsClient.cpp
+++ src/client/utilsClient.cpp
@@ -223,7 +223,7 @@ QSplashScreen* utils::infoScreen(const QString & msg) {
   QSplashScreen* screen = new QSplashScreen(QPixmap(":images/built-in/loading-screen.png"));
   screen->showMessage(msg, Qt::AlignJustify|Qt::AlignVCenter);
   screen->show();
-  return  screen;
+  return screen;
 }
 
 QString utils::getWelcomeMsg(const QString& utility)
