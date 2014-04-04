b60a435f031a01249b882c6588bf1f6c7d9e070e
rchakode <rodrigue.chakode@gmail.com>
2014-04-04 09:27:25 +0200

    improvement for the fix for ticket #27

--- core/src/DashboardBase.cpp
+++ core/src/DashboardBase.cpp
@@ -539,8 +539,11 @@ void DashboardBase::processZnsReply(QNetworkReply* _reply, SourceT& src)
           QScriptValue citem = components.value();
           QString cname = citem.property("name").toString();
           QScriptValue device = citem.property("device");
-          //QString duid = device.property("uid").toString();
-          QString dname = device.property("name").toString();//ZnsHelper::getDeviceName(duid);
+          QString dname = device.property("name").toString();
+          if (dname.isEmpty()) {
+            QString duid = device.property("uid").toString();
+            ZnsHelper::getDeviceName(duid);
+          }
           QString chkid = ID_PATTERN.arg(dname, cname);
           check.id = chkid.toStdString();
           check.host = dname.toStdString();
