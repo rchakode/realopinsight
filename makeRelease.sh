b5d68adaf15e011fa28ad9962f27aa7471c1ddba
Chakode <rodrigue.chakode@gmail.com>
2014-04-07 19:14:23 +0200

    updated makeRelease.sh

--- makeRelease.sh
+++ makeRelease.sh
@@ -3,9 +3,10 @@
 set -e 
 set -u
 
+lrelease ngrt4n.pro
 qmake ngrt4n.pro -r  CONFIG+=gui-base CONFIG+=dist 1>/dev/null
 make dist 1>/dev/null
 
-#git pull --rebase
-#git tag $1 master
-#git push --tags
+git pull --rebase
+git tag $1 master
+git push --tags
