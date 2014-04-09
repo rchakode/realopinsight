746ab5ce558472a67344bcdbf021c9f93b009daa
rchakode <rodrigue.chakode@gmail.com>
2014-04-09 22:17:30 +0200

    fixed tree path icon

--- core/src/utilsCore.cpp
+++ core/src/utilsCore.cpp
@@ -141,19 +141,19 @@ int ngrt4n::computeSeverity2Propagate(const qint8& _critValue, const qint8& prop
 
 QString ngrt4n::getIconPath(int _severity)
 {
-  QString ipath("/images/built-in/unknown.png");
+  QString ipath("images/built-in/unknown.png");
   switch (static_cast<ngrt4n::SeverityT>(_severity)) {
   case ngrt4n::Normal:
-    ipath = "/images/built-in/normal.png";
+    ipath = "images/built-in/normal.png";
     break;
   case ngrt4n::Minor:
-    ipath = "/images/built-in/minor.png";
+    ipath = "images/built-in/minor.png";
     break;
   case ngrt4n::Major:
-    ipath = "/images/built-in/major.png";
+    ipath = "images/built-in/major.png";
     break;
   case ngrt4n::Critical:
-    ipath = "/images/built-in/critical.png";
+    ipath = "images/built-in/critical.png";
     break;
   default:
     break;
