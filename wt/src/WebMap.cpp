14e37da3f2c2ee1c480a9a05566822591f5a621f
rchakode <rodrigue.chakode@gmail.com>
2014-04-14 00:11:17 +0200

    fixed thumbnail path

--- wt/src/WebMap.cpp
+++ wt/src/WebMap.cpp
@@ -244,7 +244,7 @@ void WebMap::updateThumbnail(void)
 
   // Now save the image
   if (m_thumbnailPath.empty()) {
-    m_thumbnailPath=boost::filesystem::unique_path("/run/thumb-%%%%%%.svg").string();
+    m_thumbnailPath=boost::filesystem::unique_path("run/thumb-%%%%%%.svg").string();
   }
   std::ofstream output(WWW_ROOT + "/" + m_thumbnailPath);
   thumbnailImg.write(output);
