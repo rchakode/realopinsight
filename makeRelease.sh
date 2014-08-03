b77755485670cebc1b7bf86c12a0f573ad22fe1a
James <James.Eriot@realopinsight.com>
2014-08-03 17:08:16 +0200

    fixed conflict after merging release script

--- makeRelease.sh
+++ makeRelease.sh
@@ -3,10 +3,17 @@
 set -e 
 set -u
 
+version=$1
+
 lrelease ngrt4n.pro
 qmake ngrt4n.pro -r  CONFIG+=gui-base CONFIG+=dist 1>/dev/null
 make dist 1>/dev/null
 
 git pull --rebase
+<<<<<<< HEAD
 git tag --force $1 master
 git push --force --tags
+=======
+git tag $version master
+git push github-upstream $version
+>>>>>>> a209c06... updated release script
