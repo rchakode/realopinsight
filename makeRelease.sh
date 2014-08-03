c1210d89f8e98e8001e2e5aada22c4f1126c66ff
James <James.Eriot@realopinsight.com>
2014-08-03 18:27:36 +0200

    update release script

--- makeRelease.sh
+++ makeRelease.sh
@@ -10,10 +10,5 @@ qmake ngrt4n.pro -r  CONFIG+=gui-base CONFIG+=dist 1>/dev/null
 make dist 1>/dev/null
 
 git pull --rebase
-<<<<<<< HEAD
-git tag --force $1 master
-git push --force --tags
-=======
 git tag $version master
 git push github-upstream $version
->>>>>>> a209c06... updated release script
