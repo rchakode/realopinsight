b52d42478586a5afaa3e4dc503309fea2e5e7722
James <James.Eriot@realopinsight.com>
2014-08-08 22:48:39 +0200

    updated install and update scripts

--- contribs/appliance-ultimate-makepatch.sh
+++ contribs/appliance-ultimate-makepatch.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+# ------------------------------------------------------------------------ #
+# File: install-sh                                                         #
+# Copyright (c) 2014 Rodrigue Chakode (rodrigue.chakode@gmail.com)         #
+# Creation : 08-08-2014                                                    #
+#                                                                          #
+# This Software is part of RealOpInsight Ultimate.                         #
+#                                                                          #
+# See the Terms of Use: <http://legal.realopinsight.com>                   #
+#                                                                          #
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES #
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF         #
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  #
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES   #
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN    #
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  #
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           #
+#                                                                          #
+#--------------------------------------------------------------------------#
 
 TARGET_VERSION=2014b3
 PATCH_TARBALL=patch_${TARGET_VERSION}.tar.gz
@@ -7,4 +25,4 @@ REALOPINSIGHT_WWW=/var/www
 REALOPINSIGHT_WWW_USER=www-data 
 REALOPINSIGHT_WWW_GROUP=www-data
 
-tar --same-owner zcf ${PATCH_TARBALL} ${REALOPINSIGHT_WWW}/realopinsight
+tar --same-owner -zcf ${PATCH_TARBALL} ${REALOPINSIGHT_WWW}/realopinsight
--- contribs/appliance-ultimate-upgrade.sh
+++ contribs/appliance-ultimate-upgrade.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+# ------------------------------------------------------------------------ #
+# File: install-sh                                                         #
+# Copyright (c) 2014 Rodrigue Chakode (rodrigue.chakode@gmail.com)         #
+# Creation : 08-08-2014                                                    #
+#                                                                          #
+# This Software is part of RealOpInsight Ultimate.                         #
+#                                                                          #
+# See the Terms of Use: <http://legal.realopinsight.com>                   #
+#                                                                          #
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES #
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF         #
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  #
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES   #
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN    #
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  #
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           #
+#                                                                          #
+#--------------------------------------------------------------------------#
+
 
 REALOPINSIGHT_WWW_USER=www-data 
 REALOPINSIGHT_WWW_GROUP=www-data
@@ -6,6 +25,29 @@ REALOPINSIGHT_WWW_GROUP=www-data
 TARGET_VERSION=2014b3
 PATCH_TARBALL=patch_${TARGET_VERSION}.tar.gz
 
+
+prompt_copyright()
+{ 
+  echo
+  echo "Thanks for choosing RealOpInsight Ultimate."
+  echo
+  echo "By installing the software you accept the Terms of Use"
+  echo "which a copy is available at the following address:"
+  echo "            <http://legal.realopinsight.com/>"
+  echo
+  echo "Type 'y' to accept this license offer."
+  echo "Type 'n' to decline this license offer."
+  echo "Type 'r' to view this license offer."
+  echo
+  while true; do
+    read -p "Do you accept the terms of the License ? y/N/r " rep
+    case $rep in
+	  r) w3m http://legal.realopinsight.com/ ; echo ;;
+	  y) break ;;
+	  *) exit 0 ;; 
+    esac
+  done
+}
 
 check_exit_code()
 {
@@ -16,6 +58,9 @@ check_exit_code()
     exit 1
   fi
 }
+
+
+prompt_copyright
 
 echo "DEBUG : Upgrading RealOpInsight Ultimate to version ${TARGET_VERSION}..."
 
@@ -30,8 +75,8 @@ su - ${REALOPINSIGHT_WWW_USER} -c'echo "ALTER TABLE user ADD COLUMN authsystem i
 
 check_exit_code
 
-echo -n "DEBUG : Updating binaries from patch ${PATCH_TARBALL}..."
-
+echo -n "DEBUG : Upplying update from ${PATCH_TARBALL}..."
+tar --same-owner zxf ${PATCH_TARBALL}
 
 echo -n "DEBUG : Restarting Apache..."
 /etc/init.d/apache2 start
