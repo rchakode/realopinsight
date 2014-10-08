388974843d2e239762070a9b14412dedd96a3ce6
James <James.Eriot@realopinsight.com>
2014-10-08 13:42:32 +0200

    added base code for curl-based QoS request manager

---
+++ wt/src/ngrt4n-reportd.cpp
@@ -0,0 +1,46 @@
+/*
+ * ngrt4n-reportd.cpp
+# ------------------------------------------------------------------------ #
+# Copyright (c) 2010-2014 Rodrigue Chakode (rodrigue.chakode@gmail.com)    #
+# Last Update : 23-03-2014                                                 #
+#                                                                          #
+# This file is part of RealOpInsight (http://RealOpInsight.com) authored   #
+# by Rodrigue Chakode <rodrigue.chakode@gmail.com>                         #
+#                                                                          #
+# RealOpInsight is free software: you can redistribute it and/or modify    #
+# it under the terms of the GNU General Public License as published by     #
+# the Free Software Foundation, either version 3 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# The Software is distributed in the hope that it will be useful,          #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+# GNU General Public License for more details.                             #
+#                                                                          #
+# You should have received a copy of the GNU General Public License        #
+# along with RealOpInsight.  If not, see <http://www.gnu.org/licenses/>.   #
+#--------------------------------------------------------------------------#
+ */
+
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:9090/sample-qos-info");
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
