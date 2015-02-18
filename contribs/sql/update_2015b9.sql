48f27f2f29156dbb52e5167b4ee5c67ffd34f281
R <Rodrigue.Chakode@gmail.com>
2015-02-18 23:06:18 +0100

    added database update script for v2015b9

---
+++ contribs/sql/update_2015b9.sql
@@ -0,0 +1,9 @@
+-- This script allows to update RealOpInsight Ultimate database
+-- Target Version: v2014b8
+-- Copyright (c) 2015 RealOpInsight Labs
+-- Author Rodrigue Chakode <Rodrigue.Chakode@gmail.com>
+-- Creation Date: 18/02/2015
+
+-- alter table user
+
+ALTER TABLE user ADD COLUMN dashboardmode int not null default 0;
