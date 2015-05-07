4b12f94c94b7cd877355b883fa8a612172fdcb6f
ngrt4n <ngrt4n@gmail.com>
2015-05-07 12:56:32 +0000

    realopinsiht-restore.sh: added restore script

---
+++ contribs/realopinsiht-restore.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# ------------------------------------------------------------------------ #
+# File: realopinsight-export.sh                                            #
+# Copyright (c) 2015 Rodrigue Chakode (rodrigue.chakode@gmail.com)         #
+# Creation : 08-05-2015                                                    #
+#                                                                          #
+# This Software is part of RealOpInsight Ultimate.                         #
+#                                                                          #
+# See the Terms of Use: <http://legal.realopinsight.com>                   #
+#                                                                          #
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES #
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF         #
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR  #
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES   #
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN    #
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF  #
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.           #
+#                                                                          #
+#--------------------------------------------------------------------------#
+
+set -u 
+set -e
+
+print_usage()
+{
+  echo "`basename $0` <backup_file>"
+}
+
+if [ $# -ne 1 ]; then
+  print_usage
+  exit 1
+fi
+
+REALOPINSIGHT_DB="/opt/realopinsight/data/realopinsight.db"
+REALOPINSIGHT_CONFIG_DIR="/opt/realopinsight/etc"
+REALOPINSIGHT_EXPORT_TARBALL=$1
+
+read -p "The restoration will reinitialize your database as well as configuration files. 
+         Do you want to continue? Type 'y' to confirm, other input to stop the script " rep
+OK="FALSE"
+case $rep in
+	y) OK="OK";;
+	*) ;; 
+esac
+    
+if [ $OK = "OK" ]; then
+  echo "Performing the restoration"
+  echo "Stopping Apache"x 
+  service apache2 stop
+  tar --same-owner -zxf  ${REALOPINSIGHT_EXPORT_TARBALL} -C /
+  echo "Restarting Apache"
+  service apache2 start 
+  echo "Restoration complete."
+else
+  echo "Leaving the restoration script. No change has been made on the system."
+fi 
+
+exit 0
+  
