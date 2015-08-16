fa15317e77cf567d7fb653e0f4a5e1e73010b1e1
R Chakode <Rodrigue.Chakode@gmail.com>
2015-08-16 13:28:25 -0700

    clean obsolete code

--- contribs/dockerfiles/container-entrypoint.sh
+++ contribs/dockerfiles/container-entrypoint.sh
@@ -4,8 +4,6 @@ set -e
 
 trap "echo TRAPed signal" HUP INT QUIT KILL TERM
 
-# start Apache service in background
-/usr/sbin/apachectl start
 
 # start realopinsight-reportd service
 service realopinsight-reportd start
@@ -13,16 +11,6 @@ service realopinsight-reportd start
 # start a bash session
 /bin/bash
 
-#echo "[Type 'quit' + Enter to exit] or run 'docker stop <container>'"
-#while [ 1 ]; do
-#  read rep
-#  if [ "$rep" = "quit" ]; then
-#    break
-#  fi
-#done
-
-echo "stopping services"
-/usr/sbin/apachectl stop
 
 echo "stopping realopinsight-reportd service"
 service realopinsight-reportd stop
